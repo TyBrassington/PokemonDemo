c8748c23d65c2f46477db1ce63c6da851be7bf31
TyBrassington <98743450+TyBrassington@users.noreply.github.com>
2023-02-20 02:51:43 -0800

    [PERF]: v0.6.10 - Improve object rendering efficiency
    Only the objects visible on the screen will be rendered and repainted. Previously, all objects on the map would be rendered and repainted at all times.

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -23,7 +23,7 @@ public class SuperObject {
         boolean isTileWithinXBounds = worldX + image.getWidth() *gp.scale> gp.player.worldX - gp.player.screenX && worldX - image.getWidth()*gp.scale < gp.player.worldX + gp.player.screenX;
         boolean isTileWithinYBounds = worldY + image.getHeight() *gp.scale> gp.player.worldY - gp.player.screenY && worldY - image.getHeight()*gp.scale < gp.player.worldY + gp.player.screenY;
 
-        //if(isTileWithinXBounds && isTileWithinYBounds) {
+        if(isTileWithinXBounds && isTileWithinYBounds) {
         g2d.drawImage(image, screenX, screenY, image.getWidth() * gp.scale, image.getHeight() * gp.scale, null);
 /*
         g2d.setColor(new Color(255, 0,0,120));
@@ -33,7 +33,7 @@ public class SuperObject {
         int solidAreaHeight = hitBoxArea.height;
         g2d.fillRect(solidAreaX, solidAreaY, solidAreaWidth, solidAreaHeight);
 */
-        //}
+        }
 
     }
 }
